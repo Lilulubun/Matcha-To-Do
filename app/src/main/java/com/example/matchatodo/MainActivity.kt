96ea96fce61cdef2168066844a2af864ec8ab2c3
Lilulubun <nawalhaq11@gmail.com>
2025-12-15 11:13:21 +0700

    initial commit

--- app/src/main/java/com/example/matchatodo/MainActivity.kt
+++ app/src/main/java/com/example/matchatodo/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.matchatodo.ui.theme.MatchaGreen
+//commit
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
